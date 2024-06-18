6d9951393fc974738d04ca05013cf0b1355cef88
Firekiking <kgalaxie84@gmail.com>
2024-06-18 15:28:57 +0200

    Front login terminé

---
+++ front/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React, { useState } from 'react';
+import './App.css';
+import LoginMenu from "./LoginMenu"
+
+function App() {
+    return (
+	<>
+	    <LoginMenu/>
+	</>
+    )
+}
+
+export default App
---
+++ front/src/LoginMenu.jsx
@@ -0,0 +1,112 @@
+//LoginMenu.js
+import React, { useState } from 'react';
+import { Box, TextField, Button} from '@mui/material';
+import './LoginMenu.css';
+
+function LoginMenu() {
+    const [username, setUsername] = useState('');
+    const [colorUsername, setColorUsername] = useState('')
+    const [errorMsgUsername, setErrorMsgUsername] = useState('');
+    const [errorValueUsername, setErrorValueUsername] = useState(false);
+    const [password, setPassword] = useState('');
+    const [errorMsgPassword, setErrorMsgPassword] = useState('');
+    const [colorPassword, setColorPassword] = useState('')
+    const [errorValuePassword, setErrorValuePassword] = useState(false);
+    const [isLogIn, setIsLogIn] = useState(true);
+
+    const handleUsernameChange = (event) => {
+	setUsername(event.target.value);
+	if (!isLogIn && (username.length < 3 || username.length > 20))
+	{
+	    setColorUsername('error');
+	    setErrorMsgUsername('Doit être compris entre 3 et 20 caractères !');
+	    setErrorValueUsername(true);
+	}
+	else
+	{
+	    setColorUsername('success');
+	    setErrorMsgUsername('');
+	    setErrorValueUsername(false);
+	}
+    };
+
+    const handlePasswordChange = (event) => {
+	setPassword(event.target.value);
+	if (!isLogIn && (password.length < 8 || username.length > 40))
+	{
+	    setColorPassword('error');
+	    setErrorMsgPassword('Doit être compris entre 3 et 20 caractères !');
+	    setErrorValuePassword(true);
+	}
+	else
+	{
+	    setColorPassword('success');
+	    setErrorMsgPassword('');
+	    setErrorValuePassword(false);
+	}
+    };
+
+    const handleSubmit = (event) => {
+	event.preventDefault();
+	setPassword('');
+	setUsername('');
+	// Logique de connexion à ajouter ici
+	console.log('Username:', username);
+	console.log('Password:', password);
+    };
+
+    return (
+	<div className="login-container">
+	    <form onSubmit={handleSubmit} className="login-form">
+		<h2>{isLogIn ? (<>Connexion</>) : (<>Inscription</>)}</h2>
+		<div className="form-group">
+		    <TextField
+			error={errorValueUsername}
+			id={errorValueUsername ? "outlined-error-helper-text" : "username"}
+			helperText={errorMsgUsername}
+			color={colorUsername}
+			autoComplete="username"
+			label="Nom d'utilisateur"
+			type="text"
+			value={username}
+			onInput={handleUsernameChange}
+			placeholder="Enter your username"
+			required
+		    />
+		</div>
+		<div className="form-group">
+		    <TextField
+			error={errorValuePassword}
+			id={errorValuePassword ? "outlined-error-helper-text" : "username"}
+			helperText={errorMsgPassword}
+			color={colorPassword}
+			autoComplete={isLogIn ? "current-password" : "new-password"}
+			label="Mot de passe"
+			type="password"
+			value={password}
+			onInput={handlePasswordChange}
+			placeholder="Enter your password"
+			required
+		    />
+		</div>
+		<Button
+		    variant="contained"
+		    type="submit"
+		>Se connecter</Button>
+		<p>
+		    {isLogIn ? (
+			<>
+			    Pas de compte ? <a style={{ cursor: 'pointer' }} onClick={() => setIsLogIn(false)}>S'inscrire</a>
+			</>
+		    ) : (
+			<>
+			    Déjà inscrit ? <a style={{ cursor: 'pointer' }}  onClick={() => setIsLogIn(true)}>Se connecter</a>
+			</>
+		    )}
+		</p>
+	    </form>
+	</div>
+    );
+}
+
+export default LoginMenu;
